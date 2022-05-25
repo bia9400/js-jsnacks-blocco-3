b8cb56bc76634ad60a5b6d6e86752e41646e98d1
bia9400 <ferro.biagio@gmail.com>
2022-05-25 20:58:24 +0200

    bugfix

--- Snack2/app.js
+++ Snack2/app.js
@@ -1,8 +1,8 @@
 const numeroUtente=parseInt(prompt("Quanti array vuoi generare?"))
 
 let arrays=[];
-
-for(let i=0; i<numeroUtente;i++){
+let i=0;
+while( i<numeroUtente){
 let array=[];
     for(let k=0;k<10;k++){
         array[k]=Math.round(Math.random()*100)+1
@@ -10,6 +10,7 @@ let array=[];
     }
     arrays[i]=array
     console.log(array);
+    i++
 }
 
 console.log(arrays);
